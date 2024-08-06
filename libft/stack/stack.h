734e4da89d50dbf971e6be531a5d736d6d9c1a24
Imrane Halim <ihalim.professional@gmail.com>
2024-08-06 18:31:01 +0100

    fixed stack functions

--- libft/stack/stack.h
+++ libft/stack/stack.h
@@ -2,14 +2,14 @@
 
 typedef struct s_stack
 {
-	t_list *elem;
+	long	n;
+	struct s_stack *next;
 } t_stack;
 
 //////////////////////////////////////////////////
-t_stack	*ft_stnew(void);
-void	ft_stpush(t_stack *stack, void *content);
-void	*ft_stpop(t_stack *stack);
-void	*ft_stpeek(t_stack *stack);
+t_stack	*ft_stnew(long n);
+void	ft_stpush(t_stack **stack, t_stack *content);
+void	ft_stpop(t_stack **stack);
+int 	ft_stpeek(t_stack *stack);
 int		ft_stisempty(t_stack *stack);
 int		ft_stsize(t_stack *stack);
-void	ft_stclear(t_stack *stack, void (*del)(void *del));
--- libft/stack/stack_utils.c
+++ libft/stack/stack_utils.c
@@ -1,52 +1,55 @@
 #include "stack.h"
 
-t_stack		*ft_stnew(void)
+t_stack	*ft_stnew(long n)
 {
-	t_stack *stack = malloc(sizeof(t_stack));
-	if (!stack)
+	t_stack *new;
+	new = (t_stack *)malloc(sizeof(t_stack));
+	if (!new)
 		return (NULL);
-	stack->elem = NULL;
-	return (stack);
+	new->n = n;
+	new->next = NULL;
 }
 
-void		ft_stpush(t_stack *stack, void *content)
+void	ft_stpush(t_stack **stack, t_stack *elem)
 {
-	t_list *new_elem = ft_lstnew(content);
-	if (!new_elem)
+	if (!elem)
 		return ;
-	ft_lstadd_front(&stack->elem, new_elem);
+	if (*stack == NULL) {
+		*stack = elem;
+		return ;
+	}
+	elem->next = *stack;
+	*stack = elem;
 }
 
-void		*ft_stpop(t_stack *stack)
+void	ft_stpop(t_stack **stack)
 {
-	if (!stack)
-		return NULL;
-	t_list *top = stack->elem;
-	void	*content = top->content;
-	stack->elem = top->next;
-	free(top);
-	return (content);
+	t_stack *tmp;
+	if (*stack == NULL)
+		return ;
+	tmp = (*stack)->next;
+	*stack = tmp;
+	free(tmp);
 }
 
-void		*ft_stpeek(t_stack *stack)
+int		ft_stpeek(t_stack *stack)
 {
-	if (!stack || !stack->elem)
-		return (NULL);
-	return (stack->elem->content);
+	return (stack->n);
 }
 
-int			ft_stisempty(t_stack *stack)
+int		ft_stsize(t_stack *stack)
 {
-	return (!stack->elem);
+	int i;
+	i = 0;
+	while (stack)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
 }
 
-int			ft_stsize(t_stack *stack)
+int		ft_stisempty(t_stack *stack)
 {
-	return (ft_lstsize(stack->elem));
-}
-
-void		ft_stclear(t_stack *stack, void (*del)(void *))
-{
-	ft_lstclear(&stack->elem, del);
-	free(stack);
+	return (ft_stsize(stack) == 0);
 }

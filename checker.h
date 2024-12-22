9581825b00a2ed9019d3c8a986e4c5210071e59b
Imrane Halim <ihalim@e1r6p9.1337.ma>
2024-12-22 14:12:03 +0100

    fixed some norm errors

--- checker.h
+++ checker.h
@@ -6,25 +6,9 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:31:53 by ihalim            #+#    #+#             */
-/*   Updated: 2024/12/21 14:41:46 by ihalim           ###   ########.fr       */
+/*   Updated: 2024/12/22 14:11:45 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "./libft/gnl/get_next_line.h"
-
-enum e_op
-{
-	INVALID,
-	SA,
-	SB,
-	SS,
-	PA,
-	PB,
-	RA,
-	RB,
-	RR,
-	RRA,
-	RRB,
-	RRR
-};

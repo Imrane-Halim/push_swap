025376bb167bba604e41cc8bd01aa14ae13faf66
Imrane Halim <ihalim@e1r6p9.1337.ma>
2024-12-22 13:20:26 +0100

    refactoring checker.c

--- checker.c
+++ checker.c
@@ -6,84 +6,58 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:31:53 by ihalim            #+#    #+#             */
-/*   Updated: 2024/12/21 15:50:35 by ihalim           ###   ########.fr       */
+/*   Updated: 2024/12/22 13:19:03 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-enum e_op	is_valid_op(char *op)
+void	close_on_error(t_stack *a, t_stack *b, char *op)
 {
-	if (ft_strncmp(op, "sa\n", 3) == 0)
-		return (SA);
-	else if (ft_strncmp(op, "sb\n", 3) == 0)
-		return (SB);
-	else if (ft_strncmp(op, "ss\n", 3) == 0)
-		return (SS);
-	else if (ft_strncmp(op, "pa\n", 3) == 0)
-		return (PA);
-	else if (ft_strncmp(op, "pb\n", 3) == 0)
-		return (PB);
-	else if (ft_strncmp(op, "ra\n", 3) == 0)
-		return (RA);
-	else if (ft_strncmp(op, "rb\n", 3) == 0)
-		return (RB);
-	else if (ft_strncmp(op, "rr\n", 3) == 0)
-		return (RR);
-	else if (ft_strncmp(op, "rra\n", 4) == 0)
-		return (RRA);
-	else if (ft_strncmp(op, "rrb\n", 4) == 0)
-		return (RRB);
-	else if (ft_strncmp(op, "rrr\n", 4) == 0)
-		return (RRR);
-	return (INVALID);
+	free_all(a, b);
+	free(op);
+	get_next_line(-1);
+	error("Error\n");
 }
 
-void	exec_op(t_stack *a, t_stack *b, enum e_op op)
+void	exec_op(t_stack *a, t_stack *b, char *op)
 {
-	if (op == SA)
+	if (ft_strncmp(op, "sa\n", 3) == 0)
 		sa(a);
-	else if (op == SB)
+	else if (ft_strncmp(op, "sb\n", 3) == 0)
 		sb(b);
-	else if (op == SS)
+	else if (ft_strncmp(op, "ss\n", 3) == 0)
 		ss(a, b);
-	else if (op == PA)
+	else if (ft_strncmp(op, "pa\n", 3) == 0)
 		pa(a, b);
-	else if (op == PB)
+	else if (ft_strncmp(op, "pb\n", 3) == 0)
 		pb(b, a);
-	else if (op == RA)
+	else if (ft_strncmp(op, "ra\n", 3) == 0)
 		ra(a);
-	else if (op == RB)
+	else if (ft_strncmp(op, "rb\n", 3) == 0)
 		rb(b);
-	else if (op == RR)
+	else if (ft_strncmp(op, "rr\n", 3) == 0)
 		rr(a, b);
-	else if (op == RRA)
+	else if (ft_strncmp(op, "rra\n", 4) == 0)
 		rra(a);
-	else if (op == RRB)
+	else if (ft_strncmp(op, "rrb\n", 4) == 0)
 		rrb(b);
-	else if (op == RRR)
+	else if (ft_strncmp(op, "rrr\n", 4) == 0)
 		rrr(a, b);
+	else
+		close_on_error(a, b, op);
 }
 
 void	read_operations(t_stack *a, t_stack *b)
 {
-	char		*tmp;
-	enum e_op	op;
+	char	*tmp;
 
 	tmp = get_next_line(STDIN_FILENO);
 	while (tmp)
 	{
-		op = is_valid_op(tmp);
-		if (op == INVALID)
-		{
-			get_next_line(-1);
-			free(tmp);
-			free_all(a, b);
-			error("Error\n");
-		}
-		exec_op(a, b, op);
-		free(tmp);
+		exec_op(a, b, tmp);
 		tmp = get_next_line(STDIN_FILENO);
+		free(tmp);
 	}
 }
 
